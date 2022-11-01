00952d3c0ad33f1e26c92536b040749f16e0393c
Totta <juanrestrepo300532@correo.itm.edu.co>
2022-10-31 22:58:13 -0500

    Inyección conexión

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,23 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("*")
+        .AllowAnyHeader().AllowAnyMethod();
+    });
+});
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<CLUBPIPITAContext>(options =>{
+    options.UseSqlServer("name=ConexionBaseDeDatos")
+});
 
 var app = builder.Build();
 
